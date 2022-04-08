95a895dbb2dd7578d7bb61197ede694ad284cfa8
Vinicius Dacio <viniciusdacio@gmail.com>
2022-04-08 14:43:31 -0300

    Funções melhoradas e funcionando corretamente.

--- JavaScript/Bhaskara/script.js
+++ JavaScript/Bhaskara/script.js
@@ -1,46 +1,45 @@
 function calcular(){
-const valor_a = document.getElementById("a").value;
-const valor_b = document.getElementById("b").value;
-const valor_c = document.getElementById("c").value;
+    const valor_a = document.getElementById("a").value;
+    const valor_b = document.getElementById("b").value;
+    const valor_c = document.getElementById("c").value;
 
-let a = valor_a;
-let b = valor_b;
-let c = valor_c;
+    let bh = calcular_bhaskara(valor_a, valor_b, valor_c);
 
-    let d = calcular_delta(a, b, c);
-    let x1 = parseFloat(calcular_x1(a, b, d).toFixed(2));
-    let x2 = parseFloat(calcular_x2(a, b, d).toFixed(2));
-    
-    document.getElementById("x1").innerHTML = x1;
-    document.getElementById("x2").innerHTML = x2;
-    document.getElementById("d").innerHTML = d;
-
+    if(bh === -1){
+        document.getElementById("x1").innerHTML = 'Raiz negativa';
+        document.getElementById("x2").innerHTML = 'Raiz negativa';
+        document.getElementById("d").innerHTML = 0;
+    }else{
+        let x1 = parseFloat(bh[0].toFixed(2));
+        let x2 = parseFloat(bh[1].toFixed(2));
+        exibe(calcular_delta(valor_a, valor_b, valor_c), x1, x2);
+    }
+}
 
 function calcular_delta(a, b, c){
-    var d;
+    let d;
     d = (b*b) -4*a*c;
     return d;
 }
 
-function calcular_x1(a, b, delta){
-    var x1;
+function calcular_bhaskara(a, b, c){
+    let delta = calcular_delta(a, b, c);
+    let x1, x2, s;
+
+    if(d < 0){
+        return -1;
+    }
+
     x1 = (-b + Math.sqrt(delta)) / (2*a);
-    return x1;
-}
-
-function calcular_x2(a, b, delta){
-    var x2;
     x2 = (-b - Math.sqrt(delta)) / (2*a);
-    return x2;
+    
+    s = [x1 , x2];
+
+    return s;
 }
 
-// function calcular_bhaskara(a, b, c){
-//     let d = calcular_delta(a, b, c);
-//     let x1 = parseFloat(calcular_x1(a, b, d).toFixed(2));
-//     let x2 = parseFloat(calcular_x2(a, b, d).toFixed(2));
-//     let s = [x1 , x2];
-//     return s;
-// }
-
-
+function exibe(d, x1, x2){
+    document.getElementById("x1").innerHTML = x1;
+    document.getElementById("x2").innerHTML = x2;
+    document.getElementById("d").innerHTML = d;
 }
